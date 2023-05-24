cbb102ace64f19803c213096dfa9a06920d11cfd
Mary Dault <bluestar1dault@gmail.com>
2023-05-24 16:07:43 -0400

    updating files

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,15 +1,12 @@
-//dependencies
+
 const path = require('path');
 
-//routing
+
 module.exports = (app) => {
-    //creating routes
-    //GET /notes shoukd return the notes.html file
     app.get('/notes', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
 
-    //GET * should return the index.html file
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     });
